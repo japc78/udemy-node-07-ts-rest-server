4fbb8b4a4e50bbe8bf97a101b07b9d9485082f6a
japc <japc.grafico@gmail.com>
2021-03-10 20:10:08 +0100

    Routes

--- models/server.ts
+++ models/server.ts
@@ -1,13 +1,24 @@
-import express, {Application}  from 'express';
+import express, {Application}  from 'express';\
+import userRoutes from '../routes/users'
 
 class Server {
     private app: Application;
     private port: string;
+    private apiPaths = {
+        users: '/api/users'
+    }
 
     constructor() {
         this.app = express();
         this.port = process.env.PORT || '3000';
 
+        // Definir routes
+        this.routes();
+
+    }
+
+    routes() {
+        this.app.use(this.apiPaths.users, userRoutes);
     }
 
     listen() {

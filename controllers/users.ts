98f2cfd1d985dbeafaa74eb0a27d5df2bcd65809
japc <japc.grafico@gmail.com>
2021-03-11 15:06:34 +0100

    Basic CRUD

--- controllers/users.ts
+++ controllers/users.ts
@@ -1,44 +1,59 @@
 import { Request, Response } from "express";
+import User from "../models/user";
 
-export const getUsers = (req: Request, res: Response) => {
-    res.json({
-        msg: 'getUsers',
-    });
+export const getUsers = async (req: Request, res: Response) => {
+    const users = await User.findAll();
+    if (!users) return res.status(404).json({msg: `There are not any users in Database`});
+    res.json( {users} );
 }
 
-export const getUser = (req: Request, res: Response) => {
+export const getUser = async (req: Request, res: Response) => {
     const {id} = req.params;
-    res.json({
-        msg: 'getUser',
-        id
-    });
+
+    const user = await User.findByPk(id);
+    if (!user) return res.status(404).json({msg: `The user with id: ${id}, is not exists in Database`});
+    res.json({ user });
 }
 
 
-export const postUser = (req: Request, res: Response) => {
+export const postUser = async (req: Request, res: Response) => {
+    try {
+        const { body } = req;
+
+        const existsEmail = await User.findOne({
+            where: {
+                email: body.email,
+            }
+        })
+
+        if (existsEmail) return res.status(404).json({msg: `The user with email: ${body.email}, already exists in Database`});
+
+        const user = await User.create(body);
+        res.json( {user} );
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: 'Contact with administrator'
+        })
+    }
+}
+
+export const putUser = async (req: Request, res: Response) => {
+    const {id} = req.params;
     const {body} = req;
-    res.json({
-        msg: 'postUsers',
-        body
-    });
+
+    const user = await User.findByPk(id);
+    if (!user) return res.status(404).json({msg: `The user with id: ${id}, is not exists in Database`});
+    user.update(body);
+    res.json({ user });
 }
 
-export const putUser = (req: Request, res: Response) => {
+export const deleteUser = async (req: Request, res: Response) => {
     const {id} = req.params;
-    const {body} = req;
-    res.json({
-        msg: 'putUsers',
-        body,
-        id
-    });
-}
-
-export const deleteUser = (req: Request, res: Response) => {
-    const {id} = req.params;
-    res.json({
-        msg: 'deleteUsers',
-        id
-    });
+    const user = await User.findByPk(id);
+    if (!user) return res.status(404).json({msg: `The user with id: ${id}, is not exists in Database`});
+    await user.update({state: false});
+    res.json({user});
 }
 
 
